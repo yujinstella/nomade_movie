36a87ba9ec72e6845a61425caa10040fa9e4ecbb
Yujin-stella <Yujin-stella@users.noreply.github.com>
2020-07-25 02:10:00 +0900

    Updates

---
+++ precache-manifest.346ec840f1d9ae3a06d6aa5c9f7c4dde.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79191abf3e1315c23661d0e7e90e5a6d",
+    "url": "/nomade_movie/index.html"
+  },
+  {
+    "revision": "001f708110b5644fb8c3",
+    "url": "/nomade_movie/static/css/main.ca7b6bf8.chunk.css"
+  },
+  {
+    "revision": "65ce14570ffd0231707d",
+    "url": "/nomade_movie/static/js/2.8e1b050f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/nomade_movie/static/js/2.8e1b050f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "001f708110b5644fb8c3",
+    "url": "/nomade_movie/static/js/main.b39f4d3b.chunk.js"
+  },
+  {
+    "revision": "b6477e64c3392c34971f",
+    "url": "/nomade_movie/static/js/runtime-main.ff56b109.js"
+  }
+]);
