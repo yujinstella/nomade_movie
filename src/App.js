c12a84b2ff269df73d219cda132b2a1283d8cc45
Yujin-stella <Yujin-stella@users.noreply.github.com>
2020-07-25 16:34:15 +0900

    done wating all

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import React from 'react'
 import { HashRouter, Route } from 'react-router-dom'
 import About from './routes/About'
 import Home from './routes/Home'
+import Detail from './routes/Detail'
+import Navigation from './components/Navigation'
 
 
 function App() {
   //라우터의 역할   /about -> About.js 로 가. 가야되는 페이지 안내
   return (
     <HashRouter>
+      <Navigation />
       <Route path='/' exact={true} component={Home} />
       <Route path='/about' component={About} />
+      <Route path="/movie/:id" component={Detail} />
     </HashRouter>
   );
 }
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,19 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom'
 import "./Movie.css"
 
 function Movie({ id, year, title, summary, poster, genres }) {
-    return <div className="movie">
-        <img src={poster} alt={title} title={title}></img>
-        <div className="movie_data">
-            <h3 className="movie_title"> {title}</h3>
-            <h3 className="movie_year">{year}</h3>
-            <ul className="genres">
-                {genres.map((g , index)=> <li key={index} className="genres_genre">{g}</li>)}
-            </ul>
-            <p className="movie_summary">{summary.slice(0,140)}...</p>
-        </div>
-    </div>
+    return (
+        <Link to = {{
+            pathname : `/movie/${id}`,
+            state : {
+                year,
+                title,
+                summary,
+                poster,
+                genres
+            }
+        }}>
+            <div className="movie">
+                <img src={poster} alt={title} title={title}></img>
+                <div className="movie_data">
+                    <h3 className="movie_title"> {title}</h3>
+                    <h3 className="movie_year">{year}</h3>
+                    <ul className="genres">
+                        {genres.map((g, index) => <li key={index} className="genres_genre">{g}</li>)}
+                    </ul>
+                    <p className="movie_summary">{summary.slice(0, 140)}...</p>
+                </div>
+            </div>
+        </Link>)
 }
 
 Movie.propTypes = {
